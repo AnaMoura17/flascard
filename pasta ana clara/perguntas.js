952da47cbd3b9464388cf7a03e6e29ddb7a9a5e8
AnaMoura17 <ana.camargo.moura@escola.pr.gov.br>
2025-06-09 09:34:48 -0300

    Add files via upload

---
+++ pasta ana clara/perguntas.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    criaCartao(
+        'programação', 
+        'O que é JavaScript?', 
+        'O JavaScript é uma linguagem de programação'
+    );
+
+    criaCartao(
+        'Programação', 
+        'O que é CSS?', 
+        'O CSS é uma linguagem de estilização'
+    );
+
+    criaCartao(
+        'Programação', 
+        'Qual é a linguagem de programação mais antiga?', 
+        'JavaScript'
+    );
+
+    criaCartao(
+        'Programação', 
+        'O que é html?', 
+        'É uma linguagem de marcação'
+    );
+});
